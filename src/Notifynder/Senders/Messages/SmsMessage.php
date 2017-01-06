9aa90cf88e79be3f28d695cc7c9fb85287b61e9a
Tom Witkowski <dev.gummibeer@gmail.com>
2017-01-06 12:20:37 +0100

    add getter

--- src/Notifynder/Senders/Messages/SmsMessage.php
+++ src/Notifynder/Senders/Messages/SmsMessage.php
@@ -4,39 +4,52 @@ namespace Astrotomic\Notifynder\Senders\Messages;
 
 class SmsMessage
 {
-    /**
-     * The sender of the message.
-     *
-     * @var string
-     */
     protected $originator;
-
-    /**
-     * The sender of the message.
-     *
-     * @var string
-     */
     protected $recipient;
-
-    /**
-     * The recipient of the message.
-     *
-     * @var string
-     */
     protected $body;
 
     public function from($originator)
     {
-        $this->originator = $originator;
+        $this->setOriginator($originator);
     }
 
     public function to($recipient)
     {
-        $this->recipient = $recipient;
+        $this->setRecipient($recipient);
     }
 
     public function text($body)
     {
+        $this->setBody($body);
+    }
+
+    public function setOriginator($originator)
+    {
+        $this->originator = $originator;
+    }
+
+    public function setRecipient($recipient)
+    {
+        $this->recipient = $recipient;
+    }
+
+    public function setBody($body)
+    {
         $this->body = $body;
+    }
+
+    public function getOriginator()
+    {
+        return $this->originator;
+    }
+
+    public function getRecipient()
+    {
+        return $this->recipient;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
     }
 }
